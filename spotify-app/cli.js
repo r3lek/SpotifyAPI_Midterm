f5d92a8ce44f90acdf23f45d1b14cf4f2956da36
asolon11 <aly.solon11@gmail.com>
2018-03-09 00:50:40 -0800

    set up some stuff for the cli.js

--- spotify-app/cli.js
+++ spotify-app/cli.js
@@ -2,3 +2,19 @@
     --search, --help, --getMusic,.....
 */
 
+const 
+    app = require('./app'),
+    yargs = require('yargs')
+
+const flags = yargs.usage('$0: Usage <cmd> [options]')
+    .command({
+        command: 'search',
+        desc: 'searches for an item specified by the user',
+        // Not sure what to put here yet; just setting it up
+        builder: (yargs) => {
+            return yargs.option()
+        },
+        handler: (argv) => {}
+    })
+    .help('help')
+    .argv
